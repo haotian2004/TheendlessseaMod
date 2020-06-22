7beea3b26e216b787b53d34495f2b0f4082b24f7
du_yi_wu_er <m2210379455@163.com>
2020-06-22 14:19:22 +0800

    测试

--- src/main/java/com/du_yi_wu_er/theendlessseamod/Item/ItemRenderLoader.java
+++
@@ -1,9 +0,0 @@
-package com.du_yi_wu_er.theendlessseamod.Item;
-
-public class ItemRenderLoader {
-
-    public ItemRenderLoader() {
-        ItemLoader.BlockitemregisterRenders();
-        ItemLoader.ItemitemregisterRenders();
-    }
-}
