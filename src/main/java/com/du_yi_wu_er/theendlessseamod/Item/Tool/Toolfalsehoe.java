fa3cbe88435613edec5987e6709e33a4d4ba5192
末影君 <59873888+XYMoYingJun@users.noreply.github.com>
2020-06-23 19:53:10 +0800

    末影君：提交文件

--- src/main/java/com/du_yi_wu_er/theendlessseamod/Item/Tool/Toolfalsehoe.java
+++ src/main/java/com/du_yi_wu_er/theendlessseamod/Item/Tool/Toolfalsehoe.java
@@ -1,17 +1,16 @@
 package com.du_yi_wu_er.theendlessseamod.Item.Tool;
 
 import com.du_yi_wu_er.theendlessseamod.tab.Tabtheendlessseamod;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Enchantments;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
@@ -19,11 +18,12 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class Toolfalsehoe extends ItemHoe {
 
-    public static final Item.ToolMaterial FALSE_HOE = EnumHelper.addToolMaterial("false_hoe",1,6666,3.0F,0.5F,10);
+    private static final Item.ToolMaterial FALSE_HOE = EnumHelper.addToolMaterial("false_hoe",1,6666,3.0F,0.5F,10);
 
     public Toolfalsehoe(String name){
         super(FALSE_HOE);
@@ -46,17 +46,5 @@ public class Toolfalsehoe extends ItemHoe {
         if(flagIn.isAdvanced()){
             tooltip.add(I18n.format(this.getUnlocalizedName()+".1."+"tooltip"));
         }
-    }
-
-    @Override
-    public EnumActionResult onItemUse(EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-    {
-        if (ItemDye.applyBonemeal(playerIn.getHeldItem(hand),worldIn, pos))
-        {
-            worldIn.playEvent(2005, pos, 1);
-        }
-        playerIn.getHeldItem(hand).damageItem(150, playerIn);
-
-        return EnumActionResult.SUCCESS;
     }
 }
