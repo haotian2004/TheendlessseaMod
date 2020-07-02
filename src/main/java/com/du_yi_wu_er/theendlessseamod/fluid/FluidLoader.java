9a898cb20b8f049e7a568ccf8178ea341a3a1aaa
du_yi_wu_er <m2210379455@163.com>
2020-07-02 18:13:41 +0800

    独一无二:提交文件

--- src/main/java/com/du_yi_wu_er/theendlessseamod/fluid/FluidLoader.java
+++ src/main/java/com/du_yi_wu_er/theendlessseamod/fluid/FluidLoader.java
@@ -1,9 +1,20 @@
 package com.du_yi_wu_er.theendlessseamod.fluid;
 
+import com.du_yi_wu_er.theendlessseamod.Block.BlockLoader;
 import com.du_yi_wu_er.theendlessseamod.TheEndlessSeaMod;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fluids.BlockFluidBase;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class FluidLoader {
     //Fluid 实例化
@@ -19,4 +30,37 @@ public class FluidLoader {
         FluidRegistry.addBucketForFluid(fluid);
     }
 
+    //Fluid材质 注册
+    @SideOnly(Side.CLIENT)
+    public static void registerRenders() {
+
+        registerFluidRender((BlockFluidBase) BlockLoader.mercury,"fluid_mercury");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void registerFluidRender(BlockFluidBase blockFluid,String blockStateName) {
+
+        final String location = TheEndlessSeaMod.MODID+":"+blockStateName;
+        final Item itemFluid = Item.getItemFromBlock(blockFluid);
+
+        ModelLoader.setCustomMeshDefinition(itemFluid, new ItemMeshDefinition() {
+
+            @Override
+            public ModelResourceLocation getModelLocation(ItemStack stack)
+            {
+                return new ModelResourceLocation(location, "fluid");
+            }
+
+        });
+
+        ModelLoader.setCustomStateMapper(blockFluid,new StateMapperBase() {
+
+            @Override
+            protected ModelResourceLocation getModelResourceLocation(IBlockState state)
+            {
+                return new ModelResourceLocation(location,"fluid");
+            }
+
+        });
+    }
 }
